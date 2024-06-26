99f4e57e9da423658f004e8b5ddd5cc4f5b63d4a
briian334 <a6520150009@utch.edu.mx>
2024-06-26 12:15:36 -0600

    bug fix2

--- themes/lotusdocs-release/assets/docs/js/darkmode-switch.js
+++ themes/lotusdocs-release/assets/docs/js/darkmode-switch.js
@@ -1,4 +1,4 @@
-// JavaScript para cambio de modo nocturno/día
+//Low light switcher
 const mode = document.getElementById("mode");
 
 if (mode !== null) {
@@ -12,35 +12,17 @@ if (mode !== null) {
 				localStorage.setItem("theme", "light");
 				document.documentElement.removeAttribute("data-dark-mode");
 			}
-			// Recargar los comentarios de Facebook después de cambiar el tema
-			reloadFacebookComments();
 		});
-
 	mode.addEventListener("click", () => {
 		document.documentElement.toggleAttribute("data-dark-mode");
 		localStorage.setItem(
 			"theme",
 			document.documentElement.hasAttribute("data-dark-mode") ? "dark" : "light"
 		);
-		// Recargar los comentarios de Facebook después de cambiar el tema
-		reloadFacebookComments();
 	});
-
-	// Cargar el tema al iniciar la página
 	if (localStorage.getItem("theme") === "dark") {
 		document.documentElement.setAttribute("data-dark-mode", "");
 	} else {
 		document.documentElement.removeAttribute("data-dark-mode");
-	}
-
-	// Función para recargar los comentarios de Facebook
-	function reloadFacebookComments() {
-		var comments = document.getElementsByClassName("fb-comments");
-		if (comments.length > 0) {
-			var mode = localStorage.getItem("theme") === "dark" ? "dark" : "light";
-			for (var i = 0; i < comments.length; i++) {
-				comments[i].setAttribute("data-colorscheme", mode);
-			}
-		}
 	}
 }
